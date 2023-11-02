af21f6f7c09a9dfe53f7d847ab8dc7c3efeea506
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-11-02 15:07:58 +0100

    fix: V3__disbursements.sql to adopt rows with more numerical precession

--- src/main/resources/db/migration/V3__disbursements.sql
+++ src/main/resources/db/migration/V3__disbursements.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS disbursements
     reference VARCHAR(255)   NOT NULL PRIMARY KEY,
     merchant  VARCHAR(255)   NOT NULL,
     amount    NUMERIC(10, 2) NOT NULL,
-    fees      NUMERIC(5, 2)  NOT NULL,
+    fees      NUMERIC(10, 2)  NOT NULL,
     date      DATE           NOT NULL,
     settled   BOOLEAN        NOT NULL
 );
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS disbursements
 CREATE TABLE IF NOT EXISTS disbursements_orders
 (
     id         SERIAL PRIMARY KEY,
-    amount     NUMERIC(5, 2) NOT NULL,
-    commission NUMERIC(5, 2) NOT NULL,
+    amount     NUMERIC(10, 2) NOT NULL,
+    commission NUMERIC(10, 2) NOT NULL,
     created_at DATE          NOT NULL,
     reference  VARCHAR(255),
     FOREIGN KEY (reference) REFERENCES disbursements (reference)
