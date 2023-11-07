ffc081c3ab9ffe5995dc25505ca97c9d807d97ab
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-11-07 13:00:40 +0100

    build: added Stateful Postgres container to persist the state between runs

--- src/test/java/dev/aleixmorgadas/RunSeQuraApplication.java
+++ src/test/java/dev/aleixmorgadas/RunSeQuraApplication.java
@@ -1,19 +1,34 @@
 package dev.aleixmorgadas;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.List;
+
 @TestConfiguration(proxyBeanMethods = false)
 public class RunSeQuraApplication {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
+    @ConditionalOnProperty(value = "stateful", havingValue = "false", matchIfMissing = true)
+    PostgreSQLContainer<?> postgresStatelessContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.0"));
+    }
+
+    @Bean
+    @ServiceConnection
+    @ConditionalOnProperty(value = "stateful", havingValue = "true")
+    PostgreSQLContainer<?> postgresStatefulContainer() {
+        var postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.0"))
+                .withReuse(true);
+        postgres.setPortBindings(List.of("5555:5432"));
+        postgres.withFileSystemBind("./.data/postgres", "/var/lib/postgresql/data");
+        return postgres;
     }
 
     public static void main(String[] args) {
