68d1baca1662d8ef8b6357025d41b1c7ed1e8b16
Aleix Morgadas <hello@aleixmorgadas.dev>
2023-11-08 01:06:23 +0100

    fix: test on not finding merchant

--- src/test/java/dev/aleixmorgadas/orders/OrderControllerTest.java
+++ src/test/java/dev/aleixmorgadas/orders/OrderControllerTest.java
@@ -1,7 +1,10 @@
 package dev.aleixmorgadas.orders;
 
 import dev.aleixmorgadas.AbstractIntegrationTest;
+import dev.aleixmorgadas.merchants.Merchant;
+import dev.aleixmorgadas.merchants.MerchantRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,9 +21,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OrderControllerTest extends AbstractIntegrationTest {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private MerchantRepository merchantRepository;
+
+    @BeforeEach
+    void setup() {
+        merchantRepository.save(new Merchant("merchant", "merchant@example.com", "2023-10-25", "DAILY", 25.0));
+        merchantRepository.save(new Merchant("wintheiser_bernhard", "wintheiser@example.com", "2023-10-25", "DAILY", 25.0));
+        merchantRepository.save(new Merchant("christiansen_o'keefe", "christiansen_o'keefe@example.com", "2023-10-25", "DAILY", 25.0));
+    }
 
     @AfterEach
     void cleanUp() {
+        merchantRepository.deleteAll();
         orderRepository.deleteAll();
         cleanDisbursements();
     }
